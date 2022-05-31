82de8c368ae2722bbdff881a188adc1f4017d3ad
talonmyburgh <myburgh.talon@gmail.com>
2022-05-31 20:37:30 +0200

    Revert "Merge branch 'inplace_fft_pipe_reorder' into merge_test"
    This reverts commit 615bcc17caaf1404f855e5ab347976027c58d553, reversing
    changes made to 690ad0ef0d1c645ee9ba4b309bf631637451b034.

--- casper_wb_fft/fft_r2_pipe.vhd
+++ casper_wb_fft/fft_r2_pipe.vhd
@@ -164,8 +164,7 @@ begin
 				g_dont_flip_channels => g_dont_flip_channels,
 				g_nof_points         => g_fft.nof_points,
 				g_nof_chan           => g_fft.nof_chan,
-				g_ram_primitive 	 => g_ram_primitive,
-				g_in_place           => g_fft.pipe_reo_in_place
+				g_ram_primitive 	 => g_ram_primitive
 			)
 			port map(
 				clken   => clken,
